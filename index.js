89744469b6eed6f11c00284a62e00b3f808267a5
zayra05 <ximenagilrueda@gmail.com>
2023-08-27 21:36:11 -0300

    finalización task 2

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-console.log('Hello')
 
-nombre= 'XIME',
+import {data} from './data.js';
 
-console.log(nombre)
+let contenedorCards = document.getElementById('contenedorCards');
+
+for (let event of data.events) {
+   let eventcard = ` <div class="col-12 col-sm-6 col-md-4 col-xl-3">
+                        <div class="card" >
+                                <img src="${event.image}" class="card-img1" alt="${event.image}">
+                                <div class="card-body">
+                                <h5 class="card-title">${event.name} </h5>
+                                <p class="card-text">${event.description}</p>
+                                <a href="#" class="btn btn-primary">$${event.price}</a>
+                                <a href="./details.html" class="btn btn-secundary">Details</a>
+                                </div>
+                        </div>
+                    </div> ` ;
+    
+
+    contenedorCards.innerHTML += eventcard;
+}
---
+++ pastevents.js
@@ -0,0 +1,29 @@
+
+import {data} from './data.js';
+  
+let contenedorCards = document.getElementById('contenedorCards');
+
+for (let event of data.events) { 
+    if ( event.date < data.currentDate ){ 
+       insertcard (event); 
+    }
+}
+   
+
+function insertcard (event) {
+    
+        let eventcard = `<div class="col-12 col-sm-6 col-md-4 col-xl-3">
+                        <div class="card">
+                            <img src="${event.image}" class="card-img1" alt="${event.image}">
+                            <div class="card-body">
+                            <h5 class="card-title">${event.name} </h5>
+                            <p class="card-text">${event.description}</p>
+                            <a href="#" class="btn btn-primary">$${event.price}</a>
+                            <a href="./details.html" class="btn btn-secundary">Details</a>
+                            </div>
+                        </div>
+                    </div> ` ;
+
+        contenedorCards.innerHTML += eventcard;
+    
+}
